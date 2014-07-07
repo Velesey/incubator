040508155d8fb74ce6337b5bb68844a28d216ab9
Velesey <vladimir9292@list.ru>
2014-07-08 02:41:56 +0600

    кнопка отключения сигнала и регулировка крайних значений

--- incubator.ino
+++ incubator.ino
@@ -21,12 +21,21 @@ int redLed2  = 12;
 bool isDanger = false;
 bool signal = false;
 
-int maxTemp = 27;
-int minTemp = 20;
+int maxTemp = 39;
+int minTemp = 33;
+
+int maxHum = 65;
+int humPin = 10;
+
+int minTempPin = A0;
+int maxTempPin = A1;
 
 long previousMillis = 0;   
 int interval = 100;     
 
+volatile long millisSleepDanger = 0;
+long timerSleepDanger;
+bool isMute = false;
 
 void setup() {
   Serial.begin(9600); 
@@ -38,11 +47,21 @@ void setup() {
   lcd.init(); 
   lcd.backlight();
 
-  pinMode(redLed, OUTPUT); 
+  
+  pinMode(redLed, OUTPUT);
+  pinMode(redLed2, OUTPUT); 
+  pinMode(humPin, OUTPUT); 
+  pinMode(minTempPin, INPUT);
+
+  attachInterrupt(0, mute, FALLING);
+  timerSleepDanger = 300000;
 }
 void loop() {  
+  setMinMaxTemp();
   isDanger = false;
-
+  isMute = millis() < millisSleepDanger;
+  Serial.println(millis());
+  Serial.println(millisSleepDanger);
   h[0] = dht1.readHumidity();
   t[0] = dht1.readTemperature();
 
@@ -52,13 +71,15 @@ void loop() {
   h[2] = dht3.readHumidity();
   t[2] = dht3.readTemperature();
   
+
+  
   lcd.clear();
 
   for (int i = 0; i<dhtCnt; ++i){
   	if (isnan(t[i]) || isnan(h[i])) {
     Serial.println("Failed to read from DHT");
     } else {
-	  	lcd.setCursor(0, i);
+	    lcd.setCursor(0, i);
 	    lcd.print(i+1);
 	    lcd.print(": H=");
 	    lcd.print(h[i]);
@@ -68,14 +89,52 @@ void loop() {
 	    lcd.print(t[i]);
 	    lcd.print("*C");
 
-	    if (t[i] > maxTemp || t[i] < minTemp){
-	    	if (t[i] != 0)
-	    		isDanger = true;
+      Serial.print(i+1);
+      Serial.print(": H=");
+      Serial.print(h[i]);
+      Serial.print("% ");
+
+      Serial.print("T=");
+      Serial.print(t[i]);
+      Serial.println("*C");
+
+
+	    if (t[i] >= maxTemp || t[i] <= minTemp){
+	    	if (t[i] != 0 && t[i] != 255){
+          if (!isMute){
+            isDanger = true;
+            Serial.println("danger");
+          }
+        }
+	    	
 	    }
 
-    }
+    } 
 
   }
+  lcd.setCursor(0, 3);
+  lcd.print("min:");
+  lcd.print(minTemp);
+  lcd.print("*C");
+  
+  lcd.print(" max:");
+  lcd.print(maxTemp);
+  lcd.print("*C");
+  if (isMute)
+  lcd.print("(-)");
+
+  Serial.print("min:");
+  Serial.print(minTemp);
+  Serial.print("*C");
+  
+  Serial.print(" max:");
+  Serial.print(maxTemp);
+  Serial.println("*C");
+
+
+	    
+
+	 
 
  if (millis() - previousMillis > interval) {
  	previousMillis = millis();   
@@ -83,9 +142,18 @@ void loop() {
 
 
  }
-
+  digitalWrite(humPin,  h[0] >= maxHum);
   digitalWrite(redLed, isDanger && signal);
-  digitalWrite(redLed2, isDanger && signal);
+  digitalWrite(redLed2,  isDanger && signal);
 
- 
+}
+void mute()
+{
+  millisSleepDanger = millis() + timerSleepDanger;
+  Serial.println("pushed");
+}
+
+void setMinMaxTemp(){
+minTemp = map(analogRead(minTempPin), 0, 1020, 30, 35);
+maxTemp = map(analogRead(maxTempPin), 0, 1020, 35, 40);
 }
